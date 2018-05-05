95200c0fc89c774e73ada27121ac112186e2f300
Pauline Capesius <pauline.capesius@outlook.fr>
2018-05-05 16:24:40 +0200

    creation parser csv

---
+++ Questionnaire_osteopathie/src/Questionnaire/CSVParser.java
@@ -0,0 +1,24 @@
+package Questionnaire;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CSVParser {
+	  
+	public static ArrayList<ArrayList<String>> importCSV(String filename) {
+		    ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+		    String line = "";
+		    try {
+		      BufferedReader br = new BufferedReader(new FileReader(filename));
+		      while ((line = br.readLine()) != null){
+		        ArrayList<String> dataAsString = new ArrayList<String>(Arrays.asList(line.split("|")));
+		        data.add(dataAsString);
+		      }
+		      br.close();
+		    }catch(IOException e) {e.printStackTrace();}
+		    return data;
+		  }
+}
