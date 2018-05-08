4edec870915800ac94d50a042128667b651ac072
AudreyBaguette <audrey.baguette.1@ulaval.ca>
2018-05-08 18:29:08 +0200

    Verifie format csv

--- Questionnaire_osteopathie/src/Questionnaire/Fenetres.java
+++ Questionnaire_osteopathie/src/Questionnaire/Fenetres.java
@@ -10,12 +10,14 @@ import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.io.File;
+import java.io.FileFilter;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -97,10 +99,13 @@ public class Fenetres {
 					JFileChooser fileChooser = new JFileChooser();
 					if (fileChooser.showOpenDialog(btnCharger) == JFileChooser.APPROVE_OPTION) {
 					  File fichierQuestionnaire = fileChooser.getSelectedFile();
-					  questionnaire = CSVParser.importCSV(fichierQuestionnaire.getPath());
-					  lblMessageintro.setText("Fichier chargé : " + fichierQuestionnaire.getName());
-					  btnCommencer.setEnabled(true);
-					  System.out.println(questionnaire);
+					  if(fichierQuestionnaire.getName().endsWith("csv")){
+						  questionnaire = CSVParser.importCSV(fichierQuestionnaire.getPath());
+						  lblMessageintro.setText("Fichier chargé : " + fichierQuestionnaire.getName());
+						  btnCommencer.setEnabled(true);
+					  } else {
+						  lblMessageintro.setText("Le fichier " + fichierQuestionnaire.getName() + " n'a pas le bon format.");
+					  }
 					}
 				} catch(Exception exAjouter){
 					lblMessageintro.setText("Fichier invalide");
