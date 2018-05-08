2231bad0bd9816d4b04c1aa13828efc94e70e40e
Pauline Capesius <pauline.capesius@outlook.fr>
2018-05-08 10:32:01 +0200

    questiontoaskmanytimes + ispage + hasmarqueur

--- Questionnaire_osteopathie/src/Questionnaire/FormCreationHelper.java
+++ Questionnaire_osteopathie/src/Questionnaire/FormCreationHelper.java
@@ -86,6 +86,70 @@ public class FormCreationHelper {
 				
 
 			}
+		}
+		return res;
+	}
+	
+	public static boolean isquestiontoaskmanytimes(ArrayList<String> a){
+		boolean res = false;
+		if(a.size()>0) {
+			String atester = a.get(0);
+			atester = atester.trim();
+		
+			if(atester.length()>=3) {
+				if(atester.charAt(atester.length()-1)== '1' && atester.charAt(atester.length()-2)== '+'&& atester.charAt(atester.length()-3)== '+') {
+					res = true; 
+				}
+			}
+		}
+		return res;
+	}
+	
+	public static boolean isapage(ArrayList<String> a) {
+		boolean res = false;
+		if(a.size()>3) {
+			String atester = a.get(0);
+			atester = atester.trim();
+		
+			if(atester.charAt(atester.length()-1)== ']') {
+				String compare = "[PAGE]";
+				boolean difference = false;
+				for(int i = compare.length()-1; i>=0 && !difference;i--) {
+					if(atester.charAt(i)!=compare.charAt(i)) {
+						difference = true;
+					}
+				}
+				if(!difference) {
+					res = true ;
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+	public static String hasmarqueur(ArrayList<String> a) { //renvoie chaine vide si pas de marqueur, renvoie marqueur sinon
+		String res = "";
+		if(a.size()>0) {
+			String atester = a.get(0);
+			atester= atester.trim();
+			
+			if(atester.charAt(0)=='(') {
+				boolean fini = false;
+				for(int i=1;i<atester.length() && !fini;i++) {
+					if(atester.charAt(i)==')') {
+						fini = true;
+					}
+					else {
+						res = res+atester.charAt(i);
+					}
+				}
+				if(!fini) {
+					res ="";
+				}
+			}
+		}
+		
 		
 		return res;
 	}
